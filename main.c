fe60fa500c37d6fce0f844471f7b902d42eb19ed
daejin <savif4991@gmail.com>
2022-10-25 17:03:20 +0900

    number_of_times_each_philosopher_must_eat resolved

--- main.c
+++ main.c
@@ -8,29 +8,29 @@
 // 쓰레드는 스택영역은 공유하지 않는다.
 // ./philo num die eat sleep must_eat
 
-void	init_profile(t_philo_manager *manager, t_philo_args args)
+void	init_profile(t_philo_manager *manager, t_philo_args *args)
 {
 	int	i;
 
 	i = 0;
-	while (i < args.philo_num)
+	while (i < args->philo_num)
 		manager->fork[i++] = 1;
 	i = 0;
-	while (i < args.philo_num)
+	while (i < args->philo_num)
 	{
 		manager->profile[i].idx = i + 1;
 		manager->profile[i].r_eat = 0;
 		manager->profile[i].r_sleep = 0;
 		manager->profile[i].r_think = 0;
-		manager->profile[i].die_time = args.die_time;
-		manager->profile[i].eat_time = args.eat_time;
-		manager->profile[i].sleep_time = args.sleep_time;
-		manager->profile[i].eat_max = &args.must_eat_times;
+		manager->profile[i].die_time = args->die_time;
+		manager->profile[i].eat_time = args->eat_time;
+		manager->profile[i].sleep_time = args->sleep_time;
+		manager->profile[i].eat_max = &(args->must_eat_times);
 		manager->profile[i].t_flag_adr = &manager->t_flag;
 		manager->profile[i].l_fork = &(manager->fork[i]);
-		if (!i && args.philo_num == 1)
+		if (!i && args->philo_num == 1)
 			manager->profile[i].r_fork = NULL;
-		else if (i == args.philo_num - 1)
+		else if (i == args->philo_num - 1)
 			manager->profile[i].r_fork = &(manager->fork[0]);
 		else
 			manager->profile[i].r_fork = &manager->fork[i + 1];
@@ -48,10 +48,15 @@ int	is_termination(t_philo_profile *p_info, struct timeval *time)
 		pthread_mutex_unlock(p_info->mtx);
 		return (0);
 	}
+	if (!*(p_info->eat_max))
+	{
+		*(p_info->t_flag_adr) = 1;
+		pthread_mutex_unlock(p_info->mtx);
+		return (0);
+	}
 	gettimeofday(time, NULL);
 	temp = time->tv_sec / 100000 + time->tv_usec / 1000;
-	if (temp >= p_info->die_time + p_info->r_eat
-		|| !p_info->eat_max)
+	if (temp >= p_info->die_time + p_info->r_eat)
 	{
 		*(p_info->t_flag_adr) = 1;
 		printf("%lu %i died\n", temp, p_info->idx);
@@ -183,7 +188,7 @@ int	main(int argc, char **argv)
 	manager.fork = (int *)malloc(sizeof(int) * args.philo_num);
 	if (!manager.fork)
 		return (1);
-	init_profile(&manager, args);
+	init_profile(&manager, &args);
 	if (get_thread_mutex(args, manager.profile))
 		return (1);
 	i = 0;
